46494e22b6d0a7dd9d28419cff5e11d1aa3785ef
root <israel_batista.am@hotmail.com>
2017-08-24 10:57:50 -0400

    chabge files

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ router.get("/request-token", function(req, res) {
                     if (err)
                         res.status(500).send(err);
                     else{
-                        res.setHeader("Set-Cookie", ["json="+JSON.stringify(user)]);
+                        res.setHeader("Set-Cookie", ["user="+JSON.stringify(user)]);
                         res.redirect("https://votingapp-isrmm.herokuapp.com/");
                     }
                 });
