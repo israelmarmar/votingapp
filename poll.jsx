d1c3822685d829cdd588746c71c9a54bc506fea1
root <israel_batista.am@hotmail.com>
2017-08-28 11:01:10 -0400

    change files

--- poll.jsx
+++ poll.jsx
@@ -1,3 +1,43 @@
+ var jsonuser=getCookie("user");
+		
+		
+		function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+        }
+          return "";
+      }
+		
+             $(function() {
+				 
+				 
+				if(jsonuser!=="undefined"){
+					$("#sign-in-with-twitter").text(JSON.parse(jsonuser).name+" Sign out");
+					$("#mypolls").css("display","block");
+					$("#newpoll").css("display","block");
+					$("#sign-in-with-twitter").on("click", function() {
+				     window.location.href="/logout";
+                });
+					 
+				}else{
+				$("#sign-in-with-twitter").text("Sign in with Twitter");
+				$("#mypolls").css("display","none");
+				$("#newpoll").css("display","none");
+                $("#sign-in-with-twitter").on("click", function() {
+                    window.location.href = "/request-token";
+                });
+				}
+            });
+			
 var Polls = React.createClass({
   
   getInitialState: function () {
@@ -8,10 +48,14 @@ var Polls = React.createClass({
   },
   
    componentDidMount: function() {
+   var urlpath="";
+   
+   if(jsonuser!=="undefined")
+   urlpath=JSON.parse(jsonuser).screen_name;
    
   var th = this;
     this.serverRequest = 
-      axios.get('/apijson')
+      axios.get('/apijson/'+urlpath)
      
         .then(function(result) {    
       
@@ -48,4 +92,3 @@ var Polls = React.createClass({
 });
 
 ReactDOM.render(<Polls/>, document.getElementById('polllist'));
-  
